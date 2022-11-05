ef4441eebb2a00d0322ecd72360f43ae6e3adf59
SunderB <sunder@sunderb.me>
2022-11-05 14:07:47 +0000

    Load auth token

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
+authJWT = os.environ['AUTH_JWT']
 
 app = Flask(__name__)
 
