d51026cae2084bb82ebb94480608055a10b4f755
tonysken <626627983@qq.com>
2022-11-05 18:44:19 +0000

    add -file

---
+++ server/function_area/transaction.py
@@ -0,0 +1,22 @@
+import datetime
+from server.api_service.load_api import LoadApi
+
+class Transaction(object):
+
+    def __init__(self, account_id):
+        self.load_api = LoadApi()
+        self.account_id = account_id
+
+    def get_recent_transaction(self, day, amount):
+        json_response = self.load_api.get_user_all_transaction(self.account_id)
+        result = {}
+        Transactions = []
+        for transaction in json_response["Transactions"]:
+            # 2022-10-26 02:34:18
+            timestamp = datetime.datetime.strptime(transaction["timestamp"], "%Y-%M-%D %HH:%mm:%ss")
+            now_timestamp = datetime.datetime.now()
+            if (now_timestamp - timestamp).days < day and len(Transactions) < amount:
+                Transactions.append(transaction)
+        result["Transactions"] = Transactions
+        return result
+
