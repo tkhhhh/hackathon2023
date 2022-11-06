2b0671d02a568c1d0a7533af140992af2c491eee
tonysken <626627983@qq.com>
2022-11-06 04:18:11 +0000

    add some functions for analyse words

--- client/frontend/src/app/app-routing.module.ts
+++ client/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BudgetComponent} from "./pages/budget/budget.component";
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'budget', component: BudgetComponent}
 ];
 
 @NgModule({
