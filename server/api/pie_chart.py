c4c24f47b7804c63a77f03c000ff0ad10cc64145
tonysken <626627983@qq.com>
2022-11-06 11:53:56 +0000

    add some functions for analyse words

--- server/api/pie_chart.py
+++ server/api/pie_chart.py
@@ -1,4 +1,6 @@
 from server.api_service.load_api import LoadApi
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 def create_pie_chart_data(auth_JWT, account_id):
     api = LoadApi(auth_JWT)
@@ -20,6 +22,19 @@ def create_pie_chart_data(auth_JWT, account_id):
 
     return categories
 
+def generate_pie_chart(auth_JWT, account_id):
+    categories = create_pie_chart_data(auth_JWT, account_id)
+    pp = PdfPages('long.pdf')
+    fig = plt.figure(3, figsize=(4, 4))
+    ax = plt.axes([0.1, 0.1, 0.8, 0.8])
+    for x in categories.keys():
+        ax.cla()
+        labels = x
+        fracs = categories[x]
+        p = plt.pie(fracs, labels=labels)
+        fig.savefig(pp, format='pdf')
+    pp.close()
+    return {}
     
     # {
     #         "transactionUUID": "0673bca4-fbb2-46bd-aa76-36243305ceed",
